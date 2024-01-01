100949bf33c832b612e5d1dc425b421300c41d4d
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-01-01 15:23:25 -0500

    html and js work
    added lines and ids to html for functionality. started to write embedding function. next up to do - set environment variables as server side and run a successful test of createEmbeddings

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 import { openai, supabase } from './config.js';
 
-console.log("hello")
+const movieForm = document.getElementById('movie-form')
+const favMovie = document.getElementById('favorite-movie-')
+const movieMood = document.getElementById('movie-mood-')
+const funOrSerious = document.getElementById('fun-or-serious-movie-')
 
+movieForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    main(favMovie.value, movieMood.value, funOrSerious.value)
+})
+
+async function main(favMovie, movieMood, funOrSerious) {
+    try {
+        const embedding = await createEmbedding(favMovie.value, movieMood.value, funOrSerious.value)
+        // const match = await findNearestMatch(embedding)
+        console.log(favMovie.value, movieMood.value, funOrSerious.value)
+        console.log(embedding)
+    } catch (error){
+        console.error('error in main function.', error.message)
+    }
+}
+
+async function createEmbedding(favMovie, movieMood, funOrSerious){
+    const embeddingResponse = await openai.embeddings.create({
+        model: "text-embedding-ada-002",
+        favMovie, movieMood, funOrSerious
+    })
+    return embeddingResponse.data[0].embedding
+}
+
+// async function findNearestMatch(embedding){
+//     const {data} = await supabase.rpc('match_movies', {
+//         query_embedding: embedding,
+//         match_threshold: 0.5,
+//         match_count: 1
+//     })
+// }
